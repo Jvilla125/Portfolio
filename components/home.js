6474b1c7c9ae8eb9a454aa86c4521690d43549e9
jeffvilla <jvilla125@gmail.com>
2023-04-06 15:48:41 -0700

    Create a home.js component that has the content for index.js

--- components/home.js
+++ components/home.js
@@ -3,11 +3,11 @@ import Image from 'next/image';
 export default function Index() {
     return (
         <>
-        <Image
+        {/* <Image
         src="/me.jpg"
         width={250}
         height={350}
-        />
+        /> */}
             <h1>Jeffrey Villacorta</h1>
             <h3>Software Engineer</h3>
             <button type="button" class="btn btn-primary">About Me</button>
