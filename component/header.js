e77163a58d032727b25ac12b1b4893bb5cc46837
jeffvilla <jvilla125@gmail.com>
2023-04-05 23:36:10 -0700

    Create a component directory with a Header.js file to link to each links

---
+++ component/header.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Header(){
+    return<>
+    <div>
+        <h1>Home</h1>
+        <h1>About Me</h1>
+        <h1>Projects</h1>
+        <h1>Resume</h1>
+        </div>
+        </>
+}
