644c12bdfd2864a188d08c3709f1b8b475568ff5
jeffvilla <jvilla125@gmail.com>
2023-04-17 18:25:35 -0700

    update projects page to have links to github and project website

--- components/about.js
+++ components/about.js
@@ -15,7 +15,15 @@ export default function AboutMe() {
                     />
                     <br />
                     <h5 class="card-title text-center">Jeffrey Villacorta</h5>
-                    <p class="text-center">
+                    <hr/>
+                    <p class="card-text">I am a full stack software engineer that is super eager to learn and is always taking on new challenges. <br/> <br/> 
+                    I previously worked for a commercial general contractor as a <u>project engineer</u> for over two years but decided that I wanted to make a career transition 
+                    from construction management to software engineering. <br/><br/>
+                    I recently graduated from <a target="_blank" href=" https://generalassemb.ly/education/software-engineering-immersive?&topic=&mkt_account_id=1056949875&mkt_campaign_id=1741487161&mkt_ad_group_id=68632981619&mkt_device_type=c&mkt_keyword=software%20engineering%20general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=549295011515&mkt_network=g&mkt_target_id=kwd-865210826802&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:LAX:BRTP:COD:SoftwareEngineering:GeneralAssembly&utm_content=PDP-FT&utm_term=software%20engineering%20general%20assembly&gclid=CjwKCAjw0N6hBhAUEiwAXab-TWTxm97rPeMQIgYH1lSWCqblJXDbU4-ecl6tj-HO18RUAaQD8UEIYhoC7wgQAvD_BwE&gclsrc=aw.ds">General Assembly's </a> 
+                    software engineering bootcamp in October 2022 and I am continuing to learn new languages and frameworks. <br/><br/>
+                    </p>
+                </div>
+                <p class="text-center">
                         <a class="btn btn-outline-light " data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                             Fun Facts About Me <i class="bi bi-arrow-down-short" style={{fontSize: "25px"}}></i>
                         </a>
@@ -29,15 +37,8 @@ export default function AboutMe() {
                             </ul>
                         </div>
                     </div>
-                    <hr/>
-                    <p class="card-text">I am a full stack software engineer that is super eager to learn and is always taking on new challenges. <br/> <br/> 
-                    I previously worked for a commercial general contractor as a <u>project engineer</u> for over two years but decided that I wanted to make a career transition 
-                    from construction management to software engineering. <br/><br/>
-                    I recently graduated from <a target="_blank" href=" https://generalassemb.ly/education/software-engineering-immersive?&topic=&mkt_account_id=1056949875&mkt_campaign_id=1741487161&mkt_ad_group_id=68632981619&mkt_device_type=c&mkt_keyword=software%20engineering%20general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=549295011515&mkt_network=g&mkt_target_id=kwd-865210826802&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:LAX:BRTP:COD:SoftwareEngineering:GeneralAssembly&utm_content=PDP-FT&utm_term=software%20engineering%20general%20assembly&gclid=CjwKCAjw0N6hBhAUEiwAXab-TWTxm97rPeMQIgYH1lSWCqblJXDbU4-ecl6tj-HO18RUAaQD8UEIYhoC7wgQAvD_BwE&gclsrc=aw.ds">General Assembly's </a> 
-                    software engineering bootcamp in October 2022 and I am continuing to learn new languages and frameworks. <br/><br/>
-                    </p>
-                </div>
             </div>
+            <br/>
         </>
     )
 }
--- components/project.js
+++ components/project.js
@@ -1,9 +1,9 @@
 import Image from "next/image"
+import Link from "next/link";
 
 export default function Projects() {
     return (
         <>
-
             <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active text" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Software Engineer Projects</button>
@@ -11,105 +11,104 @@ export default function Projects() {
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Construction</button>
                 </li>
-
             </ul>
             <div class="tab-content text-center" id="myTabContent">
                 <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-
-                    <ul class="list-unstyled">
-                        <li class="media">
+                    {/* Project info below  */}
+                    <br />
+                    <div class="card mx-auto" style={{ width: "30rem" }}>
+                        <h5 class="card-header">Connect Four</h5>
+                        <div class="card-body">
                             <Image
-                                class="mr-3"
-                                src="/ucsc.jpg"
+                                class="img-fluid rounded-start"
+                                src="/connectfour.png"
                                 width={250}
                                 height={350}
                             />
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">List-based media object</h5>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </li>
-                        <li class="media my-4">
+                            <p class="card-text">An online game of connect four where you can play against your friend!</p>
+                            <p class="card-text">Languages Used: </p>
+                            <br />
+                            <br />
+                            <Link href="https://github.com/Jvilla125/P1-Connect-Four" target="_blank"><button type="button" class="btn btn-primary">Github Link</button></Link> <vr />
+                            <Link href="https://jvilla125.github.io/P1-Connect-Four/" target="_blank"><button type="button" class="btn btn-primary">Play Connect four!</button></Link>
+                        </div>
+                    </div>
+                    {/* Project info below  */}
+                    <br />
+                    <br />
+                    <div class="card mx-auto" style={{ width: "30rem" }}>
+                        <h5 class="card-header">Travel App Tracker</h5>
+                        <div class="card-body">
                             <Image
-                                class="mr-3"
-                                src="/ucsc.jpg"
-                                width={250}
-                                height={350}
+                                class="img-fluid rounded-start"
+                                src="/travel.png"
+                                width={300}
+                                height={450}
                             />
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">List-based media object</h5>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </li>
-                        <li class="media">
-                            <Image
-                                class="mr-3"
-                                src="/ucsc.jpg"
-                                width={250}
-                                height={350}
-                            />
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">List-based media object</h5>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </li>
-                    </ul>
+                            <p class="card-text">Using google authentication, you will be able to keep track of all the trips that you may have taken over the years.</p>
+                            <p class="card-text">Languages Used: </p>
+                            <br />
+                            <br />
+                            <Link href="https://github.com/Jvilla125/Travel-App-Tracker" target="_blank"><button type="button" class="btn btn-primary">Github Link</button></Link> <vr />
+                            <Link href="https://travel-app-tracker.cyclic.app/" target="_blank"><button type="button" class="btn btn-primary">Travel App Tracker</button></Link>
+                        </div>
+                    </div>
                 </div>
                 <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                <div class="card-deck">
-                <div class="card1">
-                    {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-                    <Image
-                        class="img-fluid rounded-start"
-                        src="/ucsc.jpg"
-                        width={250}
-                        height={350}
-                    />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div class="card-deck">
+                        <div class="card1">
+                            {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+                            <Image
+                                class="img-fluid rounded-start"
+                                src="/ucsc.jpg"
+                                width={250}
+                                height={350}
+                            />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <br />
+                        <br />
+                        <div class="card1">
+                            {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+                            <Image
+                                class="img-fluid rounded-start"
+                                src="/ucsc.jpg"
+                                width={250}
+                                height={350}
+                            />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <br />
+                        <br />
+                        <div class="card1">
+                            {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+                            <Image
+                                class="img-fluid rounded-start"
+                                src="/ucsc.jpg"
+                                width={250}
+                                height={350}
+                            />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <br />
-                <br />
-                <div class="card1">
-                    {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-                    <Image
-                        class="img-fluid rounded-start"
-                        src="/ucsc.jpg"
-                        width={250}
-                        height={350}
-                    />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <br />
-                <br />
-                <div class="card1">
-                    {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-                    <Image
-                        class="img-fluid rounded-start"
-                        src="/ucsc.jpg"
-                        width={250}
-                        height={350}
-                    />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-            </div>
                 </div>
 
             </div>
 
 
-            
+
         </>
     )
 }
